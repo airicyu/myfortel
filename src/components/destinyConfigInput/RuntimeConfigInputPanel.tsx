2c6bae4721a45b643d6450ee74f365bf57a6c235
Airic <airic.yu@gmail.com>
2023-03-07 11:09:53 +0800

    add runtime config UIs

--- src/components/destinyConfigInput/RuntimeConfigInputPanel.tsx
+++ src/components/destinyConfigInput/RuntimeConfigInputPanel.tsx
@@ -1,4 +1,4 @@
-import { Segmented, Card, Slider } from "antd";
+import { Segmented, Card, Slider, Button } from "antd";
 import { defaultCalendar } from "fortel-ziweidoushu";
 import { useCallback, useState, useEffect } from "react";
 import { RuntimeConfigDataStateType } from "../../view/buildBoard/stateMapper";
@@ -6,6 +6,8 @@ import { LunarDateInput } from "./LunarDateInput";
 import { SolarDateInput } from "./SolarDateInput";
 import type { SliderMarks } from "antd/es/slider";
 import { CalendarType } from "fortel-ziweidoushu";
+import { DateTime } from "luxon";
+import { LineSeparator } from "./LineSeparator";
 
 const marks: SliderMarks = {
   0: {
@@ -171,6 +173,138 @@ export const RuntimeConfigInputPanel = (
     }
   }, [isRefresh, updateRuntimeConfig]);
 
+  const goToday = useCallback(() => {
+    setCalendarType(CalendarType.SOLAR);
+    setSolarYear(DateTime.now().year);
+    setSolarMonth(DateTime.now().month);
+    setSolarDay(DateTime.now().day);
+    setIsRefresh(true);
+  }, []);
+
+  const goPrevTenYear = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear) {
+      setLunarYear(lunarYear - 10);
+    }
+    setLunarLeap(false);
+    setIsRefresh(true);
+  }, [lunarYear, onChangeCalendarType]);
+
+  const goNextTenYear = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear) {
+      setLunarYear(lunarYear + 10);
+    }
+    setLunarLeap(false);
+    setIsRefresh(true);
+  }, [lunarYear, onChangeCalendarType]);
+
+  const goPrevYear = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear) {
+      setLunarYear(lunarYear - 1);
+    }
+    setLunarLeap(false);
+    setIsRefresh(true);
+  }, [lunarYear, onChangeCalendarType]);
+
+  const goNextYear = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear) {
+      setLunarYear(lunarYear + 1);
+    }
+    setLunarLeap(false);
+    setIsRefresh(true);
+  }, [lunarYear, onChangeCalendarType]);
+
+  const goPrevMonth = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear && lunarMonth) {
+      if (lunarMonth > 1) {
+        setLunarMonth(lunarMonth - 1);
+      } else {
+        setLunarYear(lunarYear - 1);
+        setLunarMonth(12);
+      }
+    }
+    setLunarLeap(false);
+    setIsRefresh(true);
+  }, [lunarMonth, lunarYear, onChangeCalendarType]);
+
+  const goNextMonth = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear && lunarMonth) {
+      if (lunarMonth < 12) {
+        setLunarMonth(lunarMonth + 1);
+      } else {
+        setLunarYear(lunarYear + 1);
+        setLunarMonth(1);
+      }
+    }
+    setLunarLeap(false);
+    setIsRefresh(true);
+  }, [lunarMonth, lunarYear, onChangeCalendarType]);
+
+  const goPrevDay = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear && lunarMonth && lunarDay) {
+      const solarDate = defaultCalendar.lunar2solar(
+        lunarYear,
+        lunarMonth,
+        lunarDay,
+        lunarLeap
+      );
+      const { year, month, day } = DateTime.fromObject({
+        year: solarDate.solarYear,
+        month: solarDate.solarMonth,
+        day: solarDate.solarDay,
+        hour: 0,
+        minute: 0,
+        second: 0,
+      }).minus({ day: 1 });
+      const lunarDate = defaultCalendar.solar2lunar(year, month, day);
+      setLunarYear(lunarDate.lunarYear);
+      setLunarMonth(lunarDate.lunarMonth);
+      setLunarDay(lunarDate.lunarDay);
+      setLunarLeap(lunarDate.isLeapMonth);
+      setIsRefresh(true);
+    }
+  }, [lunarDay, lunarLeap, lunarMonth, lunarYear, onChangeCalendarType]);
+
+  const goNextDay = useCallback(() => {
+    setCalendarType(CalendarType.LUNAR);
+    onChangeCalendarType(CalendarType.LUNAR);
+    if (lunarYear && lunarMonth && lunarDay) {
+      const solarDate = defaultCalendar.lunar2solar(
+        lunarYear,
+        lunarMonth,
+        lunarDay,
+        lunarLeap
+      );
+      const { year, month, day } = DateTime.fromObject({
+        year: solarDate.solarYear,
+        month: solarDate.solarMonth,
+        day: solarDate.solarDay,
+        hour: 0,
+        minute: 0,
+        second: 0,
+      }).plus({ day: 1 });
+      const lunarDate = defaultCalendar.solar2lunar(year, month, day);
+      setLunarYear(lunarDate.lunarYear);
+      setLunarMonth(lunarDate.lunarMonth);
+      setLunarDay(lunarDate.lunarDay);
+      setLunarLeap(lunarDate.isLeapMonth);
+      setIsRefresh(true);
+    }
+  }, [lunarDay, lunarLeap, lunarMonth, lunarYear, onChangeCalendarType]);
+
   return (
     <Card style={{ width: 600 }} title="流曜顯示">
       <Slider
@@ -185,65 +319,124 @@ export const RuntimeConfigInputPanel = (
         }}
         tooltip={{ formatter: null }}
       />
-      <Segmented
-        options={[
-          { label: "農曆", value: CalendarType.LUNAR },
-          { label: "西曆", value: CalendarType.SOLAR },
-        ]}
-        defaultValue={calendarType}
-        onChange={(value) => {
-          onChangeCalendarType(value);
-          setIsRefresh(true);
-        }}
-      />
-      <div
-        className="inline-block"
-        style={{ paddingLeft: 20, paddingRight: 0 }}
-      >
-        <div className={calendarType !== CalendarType.LUNAR ? "hide" : ""}>
-          <LunarDateInput
-            year={lunarYear}
-            month={lunarMonth}
-            day={lunarDay}
-            leap={lunarLeap}
-            onChangeYear={(value) => {
-              setLunarYear(value);
-              setIsRefresh(true);
-            }}
-            onChangeMonth={(value) => {
-              setLunarMonth(value);
-              setIsRefresh(true);
-            }}
-            onChangeDay={(value) => {
-              setLunarDay(value);
-              setIsRefresh(true);
-            }}
-            onChangeLeap={(value) => {
-              setLunarLeap(value);
-              setIsRefresh(true);
-            }}
-          />
-        </div>
+      <div>
+        <Segmented
+          options={[
+            { label: "農曆", value: CalendarType.LUNAR },
+            { label: "西曆", value: CalendarType.SOLAR },
+          ]}
+          value={calendarType}
+          onChange={(value) => {
+            onChangeCalendarType(value);
+            setIsRefresh(true);
+          }}
+        />
+        <div
+          className="inline-block"
+          style={{ paddingLeft: 20, paddingRight: 0 }}
+        >
+          <div className={calendarType !== CalendarType.LUNAR ? "hide" : ""}>
+            <LunarDateInput
+              year={lunarYear}
+              month={lunarMonth}
+              day={lunarDay}
+              leap={lunarLeap}
+              onChangeYear={(value) => {
+                setLunarYear(value);
+                setIsRefresh(true);
+              }}
+              onChangeMonth={(value) => {
+                setLunarMonth(value);
+                setIsRefresh(true);
+              }}
+              onChangeDay={(value) => {
+                setLunarDay(value);
+                setIsRefresh(true);
+              }}
+              onChangeLeap={(value) => {
+                setLunarLeap(value);
+                setIsRefresh(true);
+              }}
+            />
+          </div>
 
-        <div className={calendarType !== CalendarType.SOLAR ? "hide" : ""}>
-          <SolarDateInput
-            year={solarYear}
-            month={solarMonth}
-            day={solarDay}
-            onChangeYear={(value) => {
-              setSolarYear(value);
-              setIsRefresh(true);
-            }}
-            onChangeMonth={(value) => {
-              setSolarMonth(value);
-              setIsRefresh(true);
-            }}
-            onChangeDay={(value) => {
-              setSolarDay(value);
-              setIsRefresh(true);
-            }}
-          />
+          <div className={calendarType !== CalendarType.SOLAR ? "hide" : ""}>
+            <SolarDateInput
+              year={solarYear}
+              month={solarMonth}
+              day={solarDay}
+              onChangeYear={(value) => {
+                setSolarYear(value);
+                setIsRefresh(true);
+              }}
+              onChangeMonth={(value) => {
+                setSolarMonth(value);
+                setIsRefresh(true);
+              }}
+              onChangeDay={(value) => {
+                setSolarDay(value);
+                setIsRefresh(true);
+              }}
+            />
+          </div>
         </div>
+      </div>
+      <LineSeparator />
+      <div>
+        <div className="inline-block" style={{ width: "100px", float: "left" }}>
+          <Button onClick={goToday}>今日</Button>
+        </div>
+        <div className="inline-block" style={{ width: "400px", float: "left" }}>
+          <div className="inline-block" style={{ width: "90px" }}>
+            <div>
+              <Button onClick={goPrevTenYear} style={{ width: "80px" }}>
+                上十年
+              </Button>
+            </div>
+            <div style={{ marginTop: 5 }}>
+              <Button onClick={goNextTenYear} style={{ width: "80px" }}>
+                下十年
+              </Button>
+            </div>
+          </div>
+          <div className="inline-block" style={{ width: "90px" }}>
+            <div>
+              <Button onClick={goPrevYear} style={{ width: "80px" }}>
+                上年
+              </Button>
+            </div>
+            <div style={{ marginTop: 5 }}>
+              <Button onClick={goNextYear} style={{ width: "80px" }}>
+                下年
+              </Button>
+            </div>
+          </div>
+          <div className="inline-block" style={{ width: "90px" }}>
+            <div>
+              <Button onClick={goPrevMonth} style={{ width: "80px" }}>
+                上月
+              </Button>
+            </div>
+            <div style={{ marginTop: 5 }}>
+              <Button onClick={goNextMonth} style={{ width: "80px" }}>
+                下月
+              </Button>
+            </div>
+          </div>
+          <div className="inline-block" style={{ width: "90px" }}>
+            <div>
+              <Button onClick={goPrevDay} style={{ width: "80px" }}>
+                上日
+              </Button>
+            </div>
+            <div style={{ marginTop: 5 }}>
+              <Button onClick={goNextDay} style={{ width: "80px" }}>
+                下日
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div style={{ clear: "both" }}></div>
       </div>
     </Card>
   );
